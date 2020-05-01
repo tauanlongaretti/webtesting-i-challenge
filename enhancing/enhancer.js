6d011f5675286dcb20a7fe086491899ee94231ae
tauan-longaretti <tauanlongaretti@gmail.com>
2020-04-30 21:36:06 -0400

    test and method for fail inplemented

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,16 +6,21 @@ module.exports = {
 };
 
 function succeed(item) {
-  const enhancedItem = item.enhancement;
-  if(enhancedItem < 20) {
-    return { ...item, enhancement: enhancedItem + 1}
+  if(item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 }
   } else {
     return { ...item }
   }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement > 16) {
+    return { ...item, durability: item.durability - 10, enhancement: item.enhancement - 1 }
+  } else if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 }
+  } else { 
+    return { ...item, durability: item.durability - 10 }
+  }
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -13,4 +13,16 @@ describe('testing enhancer', () => {
             expect(succeed({ name: 'sword', durability: 80, enhancement: 20 })).toEqual({ name: 'sword', durability: 80, enhancement: 20 })
         })
     })
+    describe('fail()', () => {
+        it('should decrease item durability by 5 if the items enhancement is less than 15', () => {
+            expect(fail({ name: 'sword', durability: 80, enhancement: 8 })).toEqual({ name: 'sword', durability: 75, enhancement: 8 });
+        })
+        it('should decrease item durability by 10 if the items enhancement is 15 or more', () => {
+            expect(fail({ name: 'sword', durability: 80, enhancement: 15})).toEqual({ name: 'sword', durability: 70, enhancement: 15 });
+        })
+        it('should decrease item enhancement by 1 if item is greater than 16', () => {
+            expect(fail({ name: 'sword', durability: 80, enhancement: 17})).toEqual({ name: 'sword', durability: 70, enhancement: 16 });
+            expect(fail({ name: 'sword', durability: 80, enhancement: 18})).toEqual({ name: 'sword', durability: 70, enhancement: 17 });
+        })
+    })
 })
